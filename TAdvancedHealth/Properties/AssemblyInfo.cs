4d30652fab473daa319c7ad90d5c8662101fb510
tavstal <tavstal@gmail.com>
2025-07-30 14:09:13 +0200

    Update versioning in AssemblyInfo.cs to 4.0 and set FileVersion to 4.2.0

--- TAdvancedHealth/Properties/AssemblyInfo.cs
+++ TAdvancedHealth/Properties/AssemblyInfo.cs
@@ -26,10 +26,8 @@ using System.Runtime.InteropServices;
 //      Major Version
 //      Minor Version
 //      Build Number
-//      OnRevivedision
+//      Revision
 //
-// You can specify all the values or you can default the Build and OnRevivedision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.1.*")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.*")]
+[assembly: AssemblyFileVersion("4.2.0")]
+[assembly: AssemblyInformationalVersion("4.2.0")]
